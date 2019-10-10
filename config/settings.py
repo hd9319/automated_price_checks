b23e88a3e2240dd9346f0d38581d753e45bfeffd
hd9319 <henrydang26@gmail.com>
2019-10-09 22:30:04 -0400

    updated directory structure

---
+++ config/settings.py
@@ -0,0 +1,32 @@
+"""
+Settings configurations for Automated Price Check Application
+"""
+
+
+# Credentials for Sending Email Notifications
+EMAIL_SETTINGS = {
+	'USERNAME'; ''
+}
+
+# Credentials for Accessing S3 Functionalities
+S3_SETTINGS = {
+	'USERNAME': ''
+}
+
+# Credentials for Accessing Database
+DATABASE_SETTINGS = {
+	'USERNAME': ''
+}
+
+# Kafka Servers
+KAFKA_SETTINGS = {
+	'ZOOKEEPER': {
+		'HOSTS': [],
+	},
+	'PRODUCERS': {
+		'HOSTS': [],
+	},
+	'CONSUMERS': {
+		'HOSTS': [],
+	}
+}
